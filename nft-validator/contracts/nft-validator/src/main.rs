31d48a9804590d6e1aaa85797c1608ed956b9007
Xuejie Xiao <xxuejie@gmail.com>
2020-07-24 07:40:28 +0000

    Step 4: output NFT validation

--- nft-validator/contracts/nft-validator/src/main.rs
+++ nft-validator/contracts/nft-validator/src/main.rs
@@ -11,6 +11,8 @@ use core::result::Result;
 // https://doc.rust-lang.org/alloc/index.html
 use alloc::{collections::btree_set::BTreeSet, vec::Vec};
 
+use blake2b_ref::Blake2bBuilder;
+
 // Import CKB syscalls and structures
 // https://nervosnetwork.github.io/ckb-std/riscv64imac-unknown-none-elf/doc/ckb_std/index.html
 use ckb_std::{
@@ -19,7 +21,8 @@ use ckb_std::{
     default_alloc, entry,
     error::SysError,
     high_level::{
-        load_cell_lock_hash, load_cell_type_hash, load_script, load_script_hash, QueryIter,
+        load_cell_lock_hash, load_cell_type_hash, load_input, load_script, load_script_hash,
+        QueryIter,
     },
     syscalls::load_cell_data,
 };
@@ -45,6 +48,8 @@ enum Error {
     Encoding,
     // Add customized errors here...
     InvalidArgument,
+    RequireGovernanceMode,
+    InvalidNft,
 }
 
 impl From<SysError> for Error {
@@ -71,7 +76,7 @@ fn main() -> Result<(), Error> {
     governance_lock_hash.copy_from_slice(&args[0..32]);
 
     let mut input_lock_hashes = QueryIter::new(load_cell_lock_hash, Source::Input);
-    let _governance_mode = input_lock_hashes.any(|lock_hash| lock_hash == governance_lock_hash);
+    let governance_mode = input_lock_hashes.any(|lock_hash| lock_hash == governance_lock_hash);
 
     // To detect if an NFT is newly generated, we will need to first gather
     // NFTs in input cells.
@@ -89,14 +94,14 @@ fn main() -> Result<(), Error> {
             Err(err) => Err(err),
         }
     };
-    let _consumed_nfts: BTreeSet<[u8; 32]> =
+    let consumed_nfts: BTreeSet<[u8; 32]> =
         QueryIter::new(nft_data_loader, Source::GroupInput).collect();
 
     // In NFT generation, we will need to calculate a hash that includes the output
     // index of the NFT cells. Let's first loop through all output cells to find
     // the indices for all NFTs of the current type.
     let script_hash = load_script_hash()?;
-    let _output_nft_indices: Vec<usize> = QueryIter::new(
+    let output_nft_indices: Vec<usize> = QueryIter::new(
         |index, source| match load_cell_type_hash(index, source) {
             Ok(hash) => Ok((Some(hash), index)),
             Err(SysError::ItemMissing) => Ok((None, index)),
@@ -115,6 +120,34 @@ fn main() -> Result<(), Error> {
         }
     })
     .collect();
+
+    // Now we can loop through each output NFT and validate them:
+    // 1. If an NFT is found in consumed_nfts, this will be a transfer operation,
+    // no further work is needed.
+    // 2. If an NFT is not found in consumed_nfts, first, we need to ensure the
+    // script is in governance_mode, since NFT generation is only enabled in
+    // governance mode; second, we will validate that the NFT ID is exactly the
+    // blake2b hash of the first input of current transaction, and the current
+    // output index.
+    let first_input = load_input(0, Source::Input)?;
+    for nft_index in output_nft_indices {
+        let nft_id = nft_data_loader(nft_index, Source::Output)?;
+        if !consumed_nfts.contains(&nft_id) {
+            if !governance_mode {
+                return Err(Error::RequireGovernanceMode);
+            }
+            let mut blake2b = Blake2bBuilder::new(32)
+                .personal(b"ckb-default-hash")
+                .build();
+            blake2b.update(first_input.as_slice());
+            blake2b.update(&(nft_index as u64).to_le_bytes());
+            let mut hash = [0u8; 32];
+            blake2b.finalize(&mut hash[..]);
+            if hash != nft_id {
+                return Err(Error::InvalidNft);
+            }
+        }
+    }
 
     Ok(())
 }
